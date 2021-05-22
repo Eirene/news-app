7c86a470fc410acc87afcb2fecf16f4417d963b7
Eirene <sorokina333@gmail.com>
2021-05-22 14:43:25 +0300

    improve functions

--- dist/index.js
+++ dist/index.js
@@ -1,69 +1,81 @@
-(function (){
-    const apiKey = 'ec04a7b6f1eb47a5bbe728e05bffed59';
-    const apiUrl = 'https://news-api-v2.herokuapp.com';
-    const form = document.forms['newsControls'];
-    const searchInput = form.elements['search'];
+(function () {
+  const apiKey = 'ec04a7b6f1eb47a5bbe728e05bffed59';
+  const apiUrl = 'https://news-api-v2.herokuapp.com';
+  const form = document.forms['newsControls'];
+  const searchInput = form.elements['search'];
+  const selectCategory = document.querySelector('.filter-select #category');
 
-    const selectCategory = document.querySelector('.filter-select #category');
-    let newsCategory = 'general';
+  async function displayNews(mode, value) {
+    try {
+      toggleLoadingAnimation(true);
 
-    function newsService(url, callback) {
-        toggleLoadingAnimation(true);
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
+      let url;
+      if (mode === 'category') {
+        url = `${apiUrl}/top-headlines?country=us&category=${value}&apiKey=${apiKey}`;
+      } else {
+        // mode === 'search'
+        url = `${apiUrl}/everything?q=${value}&apiKey=${apiKey}`;
+      }
 
-        xhr.addEventListener('load', () => {
-            const response = JSON.parse(xhr.responseText);
-            callback(response.articles);
-            toggleLoadingAnimation(false);
-        });
-        xhr.addEventListener('error', () => {
-            toggleLoadingAnimation(false);
-            console.log('error');
-        });
+      const response = await fetch(url).then((res) => res.json());
 
-        xhr.send();
+      renderNews(response.articles, value);
+    } catch (error) {
+      console.error('Error happened during news fetching:', error);
+      // TODO: display error message
+    } finally {
+      toggleLoadingAnimation(false);
+    }
+  }
+
+  function toggleLoadingAnimation(isVisible) {
+    const newsLoader = document.querySelector('.news-loader');
+    if (isVisible) {
+      newsLoader.classList.add('flex');
+      newsLoader.classList.remove('hidden');
+    } else {
+      newsLoader.classList.add('hidden');
+      newsLoader.classList.remove('flex');
+    }
+  }
+
+  function renderNews(news, newsCategory) {
+    if (!news.length) {
+      console.error('No news');
+      return;
     }
 
-    function toggleLoadingAnimation(isVisible){
-        const newsLoader = document.querySelector('.news-loader');
-        if (isVisible) {
-            newsLoader.classList.add('flex');
-            newsLoader.classList.remove('hidden');
-        } else {
-            newsLoader.classList.add('hidden');
-            newsLoader.classList.remove('flex');
-        }
-    }
+    const newsContainer = document.querySelector('.news-container');
+    let fragment = '';
 
-    setTimeout(newsService, 2500,
-        `${apiUrl}/top-headlines?country=us&category=${newsCategory}&apiKey=${apiKey}`, renderNews);
+    news.forEach((newsItem) => {
+      const template = renderNewsTemplate(newsItem, newsCategory);
+      fragment += template;
+    });
 
-    function renderNews(news){
-        if (!news.length) {
-            console.error('No news');
-            return;
-        }
+    newsContainer.innerHTML = fragment;
+  }
 
-        const newsContainer = document.querySelector('.news-container');
-        let fragment = '';
+  function renderNewsTemplate(newsItem, newsCategory) {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
 
-        news.forEach( newsItem => {
-            const template = renderNewsTemplate(newsItem);
-            fragment += template;
-        });
+    let date = new Date(Date.parse(newsItem.publishedAt));
+    let monthsName = months[date.getMonth()];
 
-        newsContainer.insertAdjacentHTML('afterbegin', fragment);
-    }
-
-    function renderNewsTemplate(newsItem) {
-        let months = [ "January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December" ];
-
-        let date = new Date(Date.parse(newsItem.publishedAt));
-        let monthsName = months[date.getMonth()];
-
-        return `
+    return `
     <div class="py-8 px-6 lg:w-1/3">
         <div class="h-full flex items-start">
             <div class="w-12 flex-shrink-0 flex flex-col text-center leading-none">
@@ -72,7 +84,9 @@
             </div>
             <div class="flex-grow pl-6">
                 <h2 class="tracking-widest text-xs title-font font-medium text-indigo-500 uppercase mb-1">${newsCategory}</h2>
-                <h1 class="title-font text-xl font-medium text-gray-900 mb-3">${newsItem.title}</h1>
+                <h1 class="title-font text-xl font-medium text-gray-900 mb-3">${
+                  newsItem.title
+                }</h1>
                 <p class="leading-relaxed mb-5">
                     ${newsItem.description} 
                     <a class="text-indigo-500 inline-flex items-center" 
@@ -81,29 +95,30 @@
                     </a>
                 </p>
                 <a class="inline-flex items-center">
-                    <img alt="blog" src="${newsItem.urlToImage || 'https://picsum.photos/286/160'}" class="w-10 h-10 rounded-full flex-shrink-0 object-cover object-center">
-                    <span class="pl-3 break-all title-font font-medium text-gray-900">${newsItem.author || 'no author'}</span>
+                    <img alt="blog" src="${
+                      newsItem.urlToImage || 'https://picsum.photos/286/160'
+                    }" class="w-10 h-10 rounded-full flex-shrink-0 object-cover object-center">
+                    <span class="pl-3 break-all title-font font-medium text-gray-900">${
+                      newsItem.author || 'no author'
+                    }</span>
                 </a>
             </div>
         </div>
     </div>
   `;
-    }
+  }
 
-    //Change category
-    selectCategory.addEventListener('change', selectCategoryHandler);
+  // Change category
+  selectCategory.addEventListener('change', () => {
+    searchInput.value = '';
+    void displayNews('category', selectCategory.value);
+  });
 
-    function selectCategoryHandler(){
-        newsCategory = selectCategory.value;
-        newsService(`${apiUrl}/top-headlines?country=us&category=${newsCategory}&apiKey=${apiKey}`, renderNews);
-    }
+  // Search
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    void displayNews('search', searchInput.value);
+  });
 
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        newsService(`${apiUrl}/everything?q=${searchInput.value}&apiKey=${apiKey}`, renderNews);
-    });
+  void displayNews('category', 'general');
 })();
-
-
-// publishedAt: "2021-05-18T15:52:05Z"
-// url: "https://www.cnbc.com/2021/05/18/covid-vaccine-cdc-study-finds-disparities-in-coverage-between-rural-and-urban-areas.html"
